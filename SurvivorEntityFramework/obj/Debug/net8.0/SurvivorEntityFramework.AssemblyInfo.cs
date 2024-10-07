e81cbd65418f81c36b2936aadd73ed1f233ebda7
Ali Berkay BERBER <aliberkayberber@gmail.com>
2024-10-07 14:29:01 +0300

    developing

--- SurvivorEntityFramework/obj/Debug/net8.0/SurvivorEntityFramework.AssemblyInfo.cs
+++ SurvivorEntityFramework/obj/Debug/net8.0/SurvivorEntityFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurvivorEntityFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10251f3c409f7652428f965ba80e6849dd3d5c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurvivorEntityFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurvivorEntityFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
